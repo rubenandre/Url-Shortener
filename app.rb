2715a35c25a4d4fac6af3af142bcc8bb59653447
Rúben Silva <1181231@isep.ipp.pt>
2020-06-15 23:12:44 +0100

    feat(routes): implemented post route

--- app.rb
+++ app.rb
@@ -1,11 +1,25 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require_relative 'controllers/resolve_destination'
+require_relative 'controllers/store_urls'
+
+set :public_folder, 'public'
 
 class App < Sinatra::Base
   get '/' do
-    p 'Hello'
+    @cond = false
+    erb :home
   end
+
+  post '/' do
+    url = params[:urls]
+    @new_url = StoreUrls.new.store(url)
+    p @new_url
+    @cond = true
+    @local_endpoint = "http://localhost:3000/#{@new_url[0].local_parameter}"
+    erb :home
+  end
+
   get '/:local_parameter' do
     redirect ResolveLocalParameterDestination.new.redirect_to(params[:local_parameter])
   end
