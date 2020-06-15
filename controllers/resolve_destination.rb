6a030013cf2548860a81d1a421f460685aff04e7
Rúben Silva <1181231@isep.ipp.pt>
2020-06-15 21:00:49 +0100

    feat(resolve-destination): implemente controller to resolve url destination of a parameter if exists

---
+++ controllers/resolve_destination.rb
@@ -0,0 +1,18 @@
+require_relative '../models/shortens'
+
+class ResolveLocalParameterDestination
+  def redirect_to(local_parameter)
+    begin
+      destination_url = find_destination_url(local_parameter)
+      return "https://#{destination_url.redirect_url}"
+    rescue
+      return '/'
+    end
+
+  end
+
+  private
+  def find_destination_url(local_parameter)
+    Shortens.find_by!(:local_parameter => local_parameter)
+  end
+end
