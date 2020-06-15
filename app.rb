9738c56be5bbdab9308a10d8a760896a795573fa
Rúben Silva <1181231@isep.ipp.pt>
2020-06-15 21:02:00 +0100

    feat(routes): implemented route that receives the parameter to resolve to url

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require_relative 'controllers/resolve_destination'
 
 class App < Sinatra::Base
   get '/' do
     p 'Hello'
+  end
+  get '/:local_parameter' do
+    redirect ResolveLocalParameterDestination.new.redirect_to(params[:local_parameter])
   end
 end
